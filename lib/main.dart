04277be73ecd3f70c5ff21bec38b7a0061700d59
Patrizio Gasperi <patrizio.gasperi@gmail.com>
2022-12-13 08:15:02 +0100

    Added stacked background and a column

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,46 @@ class TodoApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FittedBox(
-        fit: BoxFit.fitWidth, child: Image.asset("images/bg-desktop-dark.jpg"));
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        decoration: BoxDecoration(
+            color: const HSLColor.fromAHSL(1, 235, 0.21, 0.11).toColor()),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset("images/bg-desktop-dark.jpg"),
+            ),
+            Column(
+              children: [
+                const Center(
+                  child: Text(
+                    "TODO",
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                ),
+                Container(
+                    width: 300,
+                    height: 50,
+                    color:
+                        const HSLColor.fromAHSL(1, 234, 0.11, 0.52).toColor())
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+// check background
+// BoxDecoration(
+//                       gradient: LinearGradient(
+//                           begin: Alignment.centerLeft,
+//                           end: Alignment.centerRight,
+//                           colors: [
+//                         const HSLColor.fromAHSL(1, 192, 1, 0.67).toColor(),
+//                         const HSLColor.fromAHSL(1, 280, 0.87, 0.65).toColor()
+//                       ])),
